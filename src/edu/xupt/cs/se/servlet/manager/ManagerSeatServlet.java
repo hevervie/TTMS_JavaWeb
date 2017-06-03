947c4cbc81486ca87d00d8538c4f7b695a6d8544
dreamer2018 <zhoupans_mail@163.com>
2017-06-03 12:11:58 +0800

    完成座位删除

--- src/edu/xupt/cs/se/servlet/manager/ManagerSeatServlet.java
+++ src/edu/xupt/cs/se/servlet/manager/ManagerSeatServlet.java
@@ -1,6 +1,9 @@
 package edu.xupt.cs.se.servlet.manager;
 
+import edu.xupt.cs.se.dao.SeatDAO;
 import edu.xupt.cs.se.dao.StudioDAO;
+import edu.xupt.cs.se.model.Seat;
+import edu.xupt.cs.se.model.Studio;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Shinelon on 2017/6/1.
@@ -15,14 +19,49 @@ import java.io.IOException;
 @WebServlet(name = "ManagerSeatServlet", urlPatterns = "/managers/seat/")
 public class ManagerSeatServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //影厅信息
         StudioDAO studioDAO = new StudioDAO();
+        ArrayList<Studio> studios = studioDAO.getAllStudio();
+        SeatDAO seatDAO = new SeatDAO();
+        System.out.println(studios.size());
+        if (null != request.getParameter("studio_id")) {
+            String str_id = request.getParameter("studio_id");
+            int studio_id;
+            try {
+                studio_id = Integer.parseInt(str_id);
+            } catch (NumberFormatException e) {
+                request.setAttribute("studios", studios);
+                request.getRequestDispatcher("/manager/seat_manager.jsp").forward(request, response);
+                return;
+            }
 
-
-        request.getRequestDispatcher("/manager/seat_manager.jsp").forward(request,response);
+            ArrayList<Seat> seats = seatDAO.getSeatByStudio(studio_id);
+            request.setAttribute("seats", seats);
+            request.setAttribute("studios", studios);
+            request.getRequestDispatcher("/manager/seat_manager.jsp").forward(request, response);
+        } else if (null != request.getParameter("type") && request.getParameter("type").equals("delete")) {
+            String str_id = request.getParameter("id");
+            int id;
+            try {
+                id = Integer.parseInt(str_id);
+            } catch (NumberFormatException e) {
+                request.setAttribute("message", "删除失败");
+                request.getRequestDispatcher("/manager/seat_manager.jsp").forward(request, response);
+                return;
+            }
+            if (seatDAO.delete(id)) {
+                //删除成功
+                response.sendRedirect("/managers/seat/");
+            } else {
+                request.setAttribute("message", "删除失败");
+                request.getRequestDispatcher("/manager/studio_manager.jsp").forward(request, response);
+            }
+        } else {
+            request.setAttribute("studios", studios);
+            request.getRequestDispatcher("/manager/seat_manager.jsp").forward(request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+        doPost(request, response);
     }
 }
--- src/edu/xupt/cs/se/servlet/manager/ManagerStudioAddServlet.java
+++ src/edu/xupt/cs/se/servlet/manager/ManagerStudioAddServlet.java
@@ -1,6 +1,8 @@
 package edu.xupt.cs.se.servlet.manager;
 
+import edu.xupt.cs.se.dao.SeatDAO;
 import edu.xupt.cs.se.dao.StudioDAO;
+import edu.xupt.cs.se.model.Seat;
 import edu.xupt.cs.se.model.Studio;
 
 import javax.servlet.ServletException;
@@ -16,20 +18,20 @@ import java.io.IOException;
 @WebServlet(name = "ManagerStudioAddServlet", urlPatterns = "/managers/studio/add/")
 public class ManagerStudioAddServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(null == request.getParameter("studio_name")){
-            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request,response);
+        if (null == request.getParameter("studio_name")) {
+            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request, response);
             return;
         }
         String name = request.getParameter("studio_name");
         String str_row = request.getParameter("studio_row");
         String str_col = request.getParameter("studio_col");
-        int row,col;
+        int row, col;
 
-        System.out.println("1"+name);
+        System.out.println("1" + name);
         // 判断name
-        if(name.equals("") || name.length() > 40){
-            request.setAttribute("message","影厅名称为空或太长！");
-            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request,response);
+        if (name.equals("") || name.length() > 40) {
+            request.setAttribute("message", "影厅名称为空或太长！");
+            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request, response);
             return;
         }
 
@@ -37,23 +39,43 @@ public class ManagerStudioAddServlet extends HttpServlet {
         try {
             row = Integer.parseInt(str_row);
             col = Integer.parseInt(str_col);
-        }catch (NumberFormatException n){
-            request.setAttribute("message","行数或列数输入错误！");
-            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request,response);
+        } catch (NumberFormatException n) {
+            request.setAttribute("message", "行数或列数输入错误！");
+            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request, response);
             return;
         }
 
         Studio studio = new Studio();
         StudioDAO studioDAO = new StudioDAO();
+        SeatDAO seatDAO = new SeatDAO();
         studio.setName(name);
         studio.setRow(row);
         studio.setCol(col);
         studio.setTheater_id(1);
-        studioDAO.insert(studio);
-        response.sendRedirect("/managers/studio/");
+
+        if (studioDAO.insert(studio)) {
+            Studio s = studioDAO.getStudioByName(name);
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= col; j++) {
+                    Seat seat = new Seat();
+                    seat.setRow(i);
+                    seat.setCol(j);
+                    seat.setStatus(0);
+                    seat.setStudio_id(s.getId());
+                    seatDAO.insert(seat);
+                }
+            }
+            response.sendRedirect("/managers/studio/");
+        } else {
+            request.setAttribute("message", "插入失败！");
+            request.getRequestDispatcher("/manager/studio_add.jsp").forward(request, response);
+            return;
+        }
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+        doPost(request, response);
     }
 }
